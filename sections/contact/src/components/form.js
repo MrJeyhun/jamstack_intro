4266a59ee3ef000899568d577ed5a5bde7f820d6
MrJeyhun <ceyhun.zade1998@gmail.com>
2023-02-26 23:56:57 +0400

    added status logic & styles

--- sections/contact/src/components/form.js
+++ sections/contact/src/components/form.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, {useReducer} from "react";
 import * as styles from "./form.module.css";
 
-console.log("styles", styles);
+const INITIAL_STATE = {
+    name: "",
+    email: "",
+    subject: "",
+    body: "",
+    status: 'IDLE'
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'updateFieldValue':
+            return {...state, [action.field]: action.value}
+        case 'updateStatus':
+            return {...state, status: action.status}
+        default:
+            return INITIAL_STATE;
+    }
+}
 
 const Form = () => {
+    const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+    console.log("state:", state)
+
+    const updateFieldValue = field => event => {
+        dispatch({type: 'updateFieldValue', field, value: event.target.value});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    if (state.status === 'SUCCESS') {
+        return <p className={styles.success}>Message sent!</p>
+    }
+
     return (
-        <form className={styles.form}>
-            <label className={styles.label}>
-                Name
-                <input className={styles.input} type="text" name="name" />
-            </label>
-            <label className={styles.label}>
-                Email
-                <input className={styles.input} type="email" name="email" />
-            </label>
-            <label className={styles.label}>
-                Subject
-                <input className={styles.input} type="text" name="subject" />
-            </label>
-            <label className={styles.label}>
-                Body
-                <textarea className={styles.input} name="body" />
-            </label>
-            <button className={styles.button}>Send</button>
-        </form>
+        <>
+            {state.status === 'ERROR' && (
+                <p className={styles.error}>Something went wrong. Please try again.</p>
+            )}
+            <form className={`${styles.form} ${state.status === "PENDING" && styles.pending}`} onSubmit={handleSubmit}>
+                <label className={styles.label}>
+                    Name
+                    <input className={styles.input} type="text" name="name" value={state.name} onChange={updateFieldValue('name')} />
+                </label>
+                <label className={styles.label}>
+                    Email
+                    <input className={styles.input} type="email" name="email" value={state.email} onChange={updateFieldValue('email')} />
+                </label>
+                <label className={styles.label}>
+                    Subject
+                    <input className={styles.input} type="text" name="subject" value={state.subject} onChange={updateFieldValue('subject')} />
+                </label>
+                <label className={styles.label}>
+                    Body
+                    <textarea className={styles.input} name="body" value={state.body} onChange={updateFieldValue('body')} />
+                </label>
+                <button className={styles.button}>Send</button>
+            </form>
+        </>
     );
 }
 
